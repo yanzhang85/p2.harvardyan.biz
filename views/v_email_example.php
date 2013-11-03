ab0048058e62f42c7f05bbf22dfcbec547c2f9ca
Yan Zhang <yanzhang01@g.harvard.edu>
2013-11-03 00:46:31 -0700

    email confirmation in progress.

--- views/v_email_example.php
+++ views/v_email_example.php
@@ -1,12 +1,12 @@
-Dear <?= $user_name?>,
+Dear <?=$post['first_name']?> <?=$post['last_name']?>,
 
 <center><h2>Welcome to Netchat!</h2> </center>
 
 Thank you for your support! 
-
+<br><br>
 Currently you can upload your picture, <a href="http://p2.harvardyan.biz/posts/add">add new posts</a>, <a href="http://p2.harvardyan.biz/posts/users">follow others </a>on Netchat. We will explore new features soon. 
-
+<br><br>
 Let's have fun on Netchat together!
-
-Sincerely,
+<br><br>
+Sincerely,<br>
 Yan Zhang
